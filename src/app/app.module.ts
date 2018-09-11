f155f60b32c4c66738529cb8800f30ba82d9f0a8
David Vaquero <pepesan@gmail.com>
2018-09-11 22:52:40 +0200

    mejoras en ejemplos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCheckboxModule, MatIconModule,
         MatFormFieldModule, MatDatepickerModule,
-        MatNativeDateModule, MatInputModule } from '@angular/material';
+        MatNativeDateModule, MatInputModule,
+        MatRadioModule, MatSelectModule,
+        MatSliderModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MAT_DATE_LOCALE} from '@angular/material/core';
 
 import { AppComponent } from './app.component';
 
@@ -19,13 +22,21 @@ import { AppComponent } from './app.component';
     ReactiveFormsModule,
     // Importaciones de Material
     MatCheckboxModule,
+    // Campos de Form
     MatInputModule,
     MatFormFieldModule,
+    // Campos Datepicker
     MatDatepickerModule,
     MatNativeDateModule,
-    MatIconModule
+    MatIconModule,
+    // Botones de Radio
+    MatRadioModule,
+    // Select
+    MatSelectModule,
+    // Slider
+    MatSliderModule
   ],
-  providers: [],
+  providers: [{provide: MAT_DATE_LOCALE, useValue: 'es-ES'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
