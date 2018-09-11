05c4a0ecddf46f2c254893ebdb54b0f606bb5da7
David Vaquero <pepesan@gmail.com>
2018-09-11 22:33:28 +0200

    generado un datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCheckboxModule} from '@angular/material';
+import {MatCheckboxModule, MatIconModule,
+        MatFormFieldModule, MatDatepickerModule,
+        MatNativeDateModule, MatInputModule } from '@angular/material';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -12,8 +15,15 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     // Importaciones de Material
     MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
